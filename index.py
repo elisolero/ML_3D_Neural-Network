8f7a25429ab37a04d0941a7c77bee89c50538976
Your Name <you@example.com>
2018-06-26 13:26:48 +0300

    add training method + think method for Sigmoid

--- index.py
+++ index.py
@@ -21,7 +21,42 @@ class NeuralNetwork():
 
     def trainModel(self,training_set_inputs, training_set_outputs, number_of_training_iterations):
 
+        for iteration in xrange(number_of_training_iterations):
+            # Pass the training set through our neural network
+            output_from_layer_1, output_from_layer_2 = self.think(training_set_inputs)
 
+            # Calculate the error for layer 2 (The difference between the desired output
+            # and the predicted output).
+            layer2_error = training_set_outputs - output_from_layer_2
+            layer2_delta = layer2_error * self.__sigmoid_derivative(output_from_layer_2)
+
+            # Calculate the error for layer 1 (By looking at the weights in layer 1,
+            # we can determine by how much layer 1 contributed to the error in layer 2).
+            layer1_error = layer2_delta.dot(self.layer2.synaptic_weights.T)
+            layer1_delta = layer1_error * self.__sigmoid_derivative(output_from_layer_1)
+
+
+            # Calculate how much to adjust the weights by
+            layer1_adjustment = training_set_inputs.T.dot(layer1_delta)
+            layer2_adjustment = output_from_layer_1.T.dot(layer2_delta)
+
+            # Adjust the weights.
+            self.layer1.synaptic_weights += layer1_adjustment
+            self.layer2.synaptic_weights += layer2_adjustment
+
+
+    # The neural network thinks.
+    def think(self, inputs):
+        output_from_layer1 = self.__sigmoid(dot(inputs, self.layer1.synaptic_weights))
+        output_from_layer2 = self.__sigmoid(dot(output_from_layer1, self.layer2.synaptic_weights))
+        return output_from_layer1, output_from_layer2
+
+    # The neural network prints its weights
+    def print_weights(self):
+        print "    Layer 1 (4 neurons, each with 3 inputs): "
+        print self.layer1.synaptic_weights
+        print "    Layer 2 (1 neuron, with 4 inputs):"
+        print self.layer2.synaptic_weights
 
 
 
